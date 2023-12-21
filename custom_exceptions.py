2fde84602a183e2d59549c8fb43fb460ec09be41
RogerKimJazzLover <minseungkim1017@gmail.com>
2023-12-21 23:07:32 +0900

    small modifications

--- custom_exceptions.py
+++ custom_exceptions.py
@@ -4,11 +4,11 @@ class ResponseError(Exception):
 	'''
 	def __init__(self, status_code):
 		self.status_code = status_code
-		self.messaage = f"Response Error: {status_code}"
+		self.messaage = f"\nResponse Error: {status_code}"
 
 class ReAttemptFail(Exception):
 	'''
-	Custom error that is raised when the response code is not 200
+	Custom error that is raised when the reattempt function in the reusable_func fails.
 	'''
 	def __init__(self, value):
 		self.value = value

2353589171296fd1dbdd6453367a5516b01cffc8
RogerKimJazzLover <minseungkim1017@gmail.com>
2023-12-22 23:36:04 +0900

    Fatal error: insufficient 'allowed API request'

--- get_search_rate.py
+++ get_search_rate.py
@@ -1,14 +1,16 @@
 from datalab_api import DatalabSearchAPI
 import reusable_funcs
 import custom_exceptions
-from pprint import pprint
+
 from tqdm import tqdm
 import pandas as pd
-import json
+import numpy as np
+import json, time
 
-def GetResponse(datalab_api, keywords: list) -> json:
+@reusable_funcs.ReAttemptUntilFailure(max_attempt=3, time=10)
+def GetResponse(datalab_api, keywords: list, startDate: str, endDate: str) -> dict:
     keywords_groups = datalab_api.CreateKeywordsGroups(keywords)
-    body = datalab_api.CreateBody(startDate="2023-11-20", endDate="2023-12-20", keywords_groups=keywords_groups)
+    body = datalab_api.CreateBody(startDate=startDate, endDate=endDate, keywords_groups=keywords_groups)
     response = datalab_api.GetResponse(body)
     rescode = response.getcode()
 
@@ -17,8 +19,24 @@ def GetResponse(datalab_api, keywords: list) -> json:
     
     response_body = response.read()
     jobj = json.loads(response_body)
-    print(type(jobj))
     return jobj
+
+def CreateDateColumns(response, table):
+    desired_part = response["results"][0]["data"]
+    for i in range(len(desired_part)):
+        table[desired_part[i]["period"]] = []
+
+def AppendRatio(response, table):
+    dates = response["results"][0]["data"]
+    for i in range(5):
+        #For each keywords
+        for day in range(len(dates)):
+            #For each day of each keyword
+            try:
+                ratio = response["results"][i]["data"][day]["ratio"]
+                table[dates[day]["period"]].append(ratio)
+            except IndexError:
+                table[dates[day]["period"]].append(np.nan)
 
 def main():
     #1. Initializing values
@@ -26,16 +44,44 @@ def main():
     table = pd.read_csv(f"./data/m_top10_keywords.csv", encoding="euc-kr")
     keywords = list(table["Keywords"])
 
-    #2.. Getting the endDate(yesterday) and startDate(1 month ago)
+    #2. Getting the endDate(yesterday) and startDate(1 month ago)
     endDate = reusable_funcs.GetEndDate()
     startDate = reusable_funcs.GetStartDate(endDate, 'm')
     endDate = endDate.strftime('%Y-%m-%d')
     startDate = startDate.strftime('%Y-%m-%d')
 
+    #3. Initializing new data
+    new_data = {}
+    '''
+    going to look something like...
+    {
+        "2023-11-20" : [24,42,12,1,23,.......] (len = 60000)
+        "2023-11-21" : [24,42,12,1,23,.......] (len = 60000)
+        "2023-11-23" : [24,42,12,1,23,.......] (len = 60000)
+        "2023-11-24" : [24,42,12,1,23,.......] (len = 60000)
+        .
+        .
+        .
+        .
+        (len = 30)
+    }
+    '''
+
+    #4. Completing the new data table
     for i in tqdm(range(5, 6001, 5)):
         temp_keywords = keywords[i-5:i]
-        pprint(GetResponse(datalab_api, temp_keywords))
+        response = GetResponse(datalab_api, temp_keywords, startDate, endDate)
+        if i == 5:
+            CreateDateColumns(response, table=new_data)
+        AppendRatio(response, new_data)
+    
+    #5. Appending the new data to the old table
+    for key, value in new_data.items():
+        print(key)
+        print(value)
+        table[str(key)] = value
 
+    table.to_csv("./data/m_keywords_search_ratio_per_day.csv", encoding="euc-kr", index=False)
 
 if __name__ == "__main__":
     main()

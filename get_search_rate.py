a7381fb8fe532e7289ee9888a36c3571b03609ba
RogerKimJazzLover <minseungkim1017@gmail.com>
2023-12-25 00:30:44 +0900

    Finishing feature: get search rate

--- get_search_rate.py
+++ get_search_rate.py
@@ -2,6 +2,7 @@ from datalab_api import DatalabSearchAPI
 import reusable_funcs
 import custom_exceptions
 
+from tabulate import tabulate
 import json, time, os
 from tqdm import tqdm
 import pandas as pd
@@ -21,23 +22,33 @@ def GetResponse(datalab_api, keywords: list, startDate: str, endDate: str) -> di
     jobj = json.loads(response_body)
     return jobj
 
-def CreateDateColumns(response, table):
-    desired_part = response["results"][0]["data"]
-    for i in range(len(desired_part)):
-        table[desired_part[i]["period"]] = []
+def GetDaysList(startDate, endDate) -> list:
+    days_list = []
+    while startDate != endDate:
+        days_list.append(startDate.strftime('%Y-%m-%d'))
+        startDate += pd.to_timedelta(1, 'D')
+    days_list.append(endDate)
+    return days_list
 
-def AppendRatio(response, table):
-    dates = response["results"][0]["data"]
+def CreateDateColumns(days_list, table):
+    for i in range(30):
+        table[days_list[i]] = []
+
+def AppendRatio(days_list: list, response, table):
     for i in range(5):
         #For each keywords
-        for day in range(len(dates)):
+        table["Keywords"].append(response["results"][i]["title"])
+        for day in range(30):
             #For each day of each keyword
             try:
                 ratio = response["results"][i]["data"][day]["ratio"]
-                table[dates[day]["period"]].append(ratio)
+                table[days_list[day]].append(ratio)
             except IndexError:
                 print("!ERROR: INDEX ERROR, APPENDING NaN")
-                table[dates[day]["period"]].append(np.nan)
+                print(response["results"][i]["title"])
+                for i in range(day, 30):
+                    table[days_list[i]].append(np.nan)
+                break
 
 def main():
     #1. Initializing values
@@ -50,11 +61,14 @@ def main():
     #2. Getting the endDate(yesterday) and startDate(1 month ago)
     endDate = reusable_funcs.GetEndDate()
     startDate = reusable_funcs.GetStartDate(endDate, 'm')
+    days_list = GetDaysList(startDate, endDate)
     endDate = endDate.strftime('%Y-%m-%d')
     startDate = startDate.strftime('%Y-%m-%d')
 
     #3. Initializing new data
-    new_data = {}
+    new_data = {
+        "Keywords":[]
+    }
     '''
     going to look something like...
     {
@@ -66,22 +80,23 @@ def main():
     }
     '''
 
+    CreateDateColumns(days_list, table=new_data)#INITIALIZING THE COLUMNS AS THE DATES IN THE FIRST LOOP
     #4. Completing the new data table
     for i in tqdm(range(5, len(keywords)+1, 5)):
         temp_keywords = keywords[i-5:i]
         response = GetResponse(datalab_api, temp_keywords, startDate, endDate)
-        if i == 5:
-            #INITIALIZING THE COLUMNS AS THE DATES IN THE FIRST LOOP
-            CreateDateColumns(response, table=new_data)
-        AppendRatio(response, new_data)
-    print(new_data['2023-11-25'])
-    print(len(new_data['2023-11-25']))
+        AppendRatio(days_list, response, new_data)
 
+    # df_new_data = pd.DataFrame(new_data)
+    # debug_data = df_new_data[["Keywords", "2023-12-10"]]
+    # print(tabulate(debug_data, headers='keys', tablefmt='psql'))
+
+    del new_data["Keywords"]
     #5. Appending the new data to the old table
     for key, value in new_data.items():
         table[str(key)] = value
 
-    save_as = os.environ.get('SAVE_AS', './data/test.csv')
+    save_as = os.environ.get('SAVE_AS', './data/test_search_ratio.csv')
     table.to_csv(save_as, encoding="euc-kr", index=False)
 
 if __name__ == "__main__":

b68f89bf2d17cabb58f8e8b0053d9d4eac07645a
RogerKimJazzLover <minseungkim1017@gmail.com>
2023-12-28 00:21:16 +0900

    Finish feature: Get daily search

--- get_daily_search.py
+++ get_daily_search.py
@@ -2,16 +2,17 @@ from tabulate import tabulate
 import pandas as pd
 
 def main():
-    table = pd.read_csv("./data/test_search_ratio.csv", encoding="euc-kr")
+    table = pd.read_csv("./data/m_search_ratios.csv", encoding="euc-kr")
     search_rates = table.drop(table.columns[[0,1,2,3,4,5]], axis=1)
     sum = search_rates.sum(axis=1)
     x_factor = table["Monthly_num_search"] / (sum[0] / 100)
     daily_search = search_rates.mul(x_factor, axis="index") / 100
-    daily_search["Keywords"] = table["Keywords"]
+    # daily_search["Keywords"] = table["Keywords"]
 
     new_table = table.iloc[:, [0,1,2]]
-    new_table = pd.merge(left=new_table, right=daily_search, on='Keywords')
-    new_table.to_csv("./data/test_daily_search.csv", encoding = "euc-kr", index=False)
+    new_table = pd.merge(left=new_table, right=daily_search, left_index=True, right_index=True)
+    new_table = new_table.drop_duplicates(subset='Keywords')
+    new_table.to_csv("./data/m_daily_search.csv", encoding = "euc-kr", index=False)
 
 if __name__ == "__main__":
   main()

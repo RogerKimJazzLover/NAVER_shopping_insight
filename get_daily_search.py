22d089de8dfd7aff5ab0e5503412ac2a24101138
RogerKimJazzLover <minseungkim1017@gmail.com>
2024-03-27 12:41:21 +0900

    Round daily search to integer

--- get_daily_search.py
+++ get_daily_search.py
@@ -1,13 +1,25 @@
 from tabulate import tabulate
 import pandas as pd
 
+def round_to_int(daily_search: pd.DataFrame):
+    for index, row in daily_search.iterrows():
+        try:
+            daily_search.loc[index] = row.round().astype(int)
+        except:
+            for i in range(30):
+                try:
+                    row.iloc[i] = round(row.iloc[i], 0)
+                except:
+                    row.iloc[i] = row.iloc[i]
+            continue
+
 def main():
     table = pd.read_csv("./data/m_search_ratios.csv", encoding="euc-kr")
     search_rates = table.drop(table.columns[[0,1,2,3,4,5]], axis=1)
     sum = search_rates.sum(axis=1)
     x_factor = table["Monthly_num_search"] / (sum[0] / 100)
     daily_search = search_rates.mul(x_factor, axis="index") / 100
-    # daily_search["Keywords"] = table["Keywords"]
+    round_to_int(daily_search)
 
     new_table = table.iloc[:, [0,1,2]]
     new_table = pd.merge(left=new_table, right=daily_search, left_index=True, right_index=True)
